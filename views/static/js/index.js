6741e4c78e0e1de128a09a39a988bf2cab1fd066
Leandro <leandro.frigerio@devugees.org>
2019-08-08 14:57:01 +0200

    fix empty search results

--- views/static/js/index.js
+++ views/static/js/index.js
@@ -1,6 +1,4 @@
 document.querySelector("#searchForm").addEventListener('submit', event => {
     event.preventDefault();
-    console.log("submit")
-    console.log("search/age/" + document.querySelector("#searchInput").value);
-    window.location.href = "http://localhost:3000/search/age/" + document.querySelector("#searchInput").value;
+    window.location.href = "/search/age/" + document.querySelector("#searchInput").value;
 });
