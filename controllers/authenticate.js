ac29d91c7a8e2b17658763ac42e184ec1af124dd
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-08-14 08:28:25 +0200

    make login message without success

--- controllers/authenticate.js
+++ controllers/authenticate.js
@@ -1,10 +1,10 @@
 module.exports = {
   login: (req, res) => {
     console.log(`Form received: ${req.body.email}`);
-    UserModel.findOne({email:req.body,email},(err,user) =>  {
-      if(err) throw err;
-      if(!user){
-        res.json()
+    UserModel.findOne({ email: req.body, email }, (err, user) => {
+      if (err) throw err;
+      if (!user) {
+        res.json({ error: { message: "You are not allowed to enter." } })
       }
     })
   }
